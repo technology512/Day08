ebb3d2c6de0f90e232ae04951c5ffd3683ff5ba5
technology512 <ald.wapper@gmail.com>
2021-03-16 00:52:21 +0100

    functions-global vs. local scope

---
+++ ex05/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+var dress = "little dress";
+function myDress() {
+    var dress = "ball gown";
+    // Only change code above this line
+    return dress;
+}
+// Only change code below this line
+myDress();
+// Only chang ecode above this line
+console.log(dress);
+
+module.exports = myDress;
