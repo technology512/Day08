68258519b1d969357850a1fa30037bf02204d726
technology512 <ald.wapper@gmail.com>
2021-03-16 01:12:18 +0100

    functions - the parselnt()

---
+++ ex14/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+
+function convertStrToInt(str) {
+    var str;
+    return parseInt(str);
+}
+
+console.log(parseInt("44"));
+console.log(parseInt("27"));
+console.log(parseInt("JeffBezos"));
+
+// Only change code above this line
+
+module.exports = convertStrToInt;
