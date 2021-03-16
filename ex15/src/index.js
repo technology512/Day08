5cce2a05dce01dd988279ba3796fea830ae35a4c
technology512 <ald.wapper@gmail.com>
2021-03-16 01:12:56 +0100

    use the parselnt function with a radix

---
+++ ex15/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+// Only change code below this line
+
+function convertToInt(str) {
+    var str;
+    return parseInt(str);
+}
+
+console.log(parseInt("BA", 18));
+console.log(parseInt("F1", 18));
+console.log(parseInt("JeffBezos", 18));
+
+// Only change code above this line
+
+module.exports = convertToInt;
